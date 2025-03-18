59a338cfe0f5660526b19838ed4dff87f291c9be
Sophia <scoreap@correo.url.edu.gt>
2025-03-18 08:19:38 -0600

    Primer Ejercicio

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -2,6 +2,15 @@ package ed.lab;
 
 public class E01InvertBT {
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
+        if (root == null)
+            return null;
+        //Guardando los nodos en su lado opuesto
+        TreeNode left= invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        //Invirtiendo los nodos
+        root.left = right;
+        root.right = left;
         return root;
     }
 }
